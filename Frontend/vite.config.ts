76574170fd8c9556f9a04d1d2090db0ecf5ebd5f
Juan Manuel <juanmanuel.41595@gmail.com>
2026-07-13 21:10:05 -0500

    chore(front-config): instalación de entorno y configuración inicial

---
+++ Frontend/vite.config.ts
@@ -0,0 +1,17 @@
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  test: {
+    environment: 'jsdom',
+  },
+})
